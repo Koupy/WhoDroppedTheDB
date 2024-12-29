25ae207e66f29e229c49061cfde6a265d3400e47
Koupy <nanook.tech@proton.me>
2024-12-29 16:50:35 +0100

    Update displayMenu function in userInteraction.c

--- src/userInteraction.c
+++ src/userInteraction.c
@@ -3,22 +3,36 @@
 void displayMenu() {
 	size_t choice;
 
-	printf("\n--- WhoDropTheDB ---\n");
-	printf("1. Begin\n");
-	printf("2. Quit\n");
-	printf("Enter your choice: ");
-	scanf("%zu", &choice);
+	while(1) {
+        	printf("\n--- PACIFIC STANDARD ---\n");
+		printf(" -- CUSTOMER -- \n");
+		printf("1. Register a New Customer \n");
+        	printf("\n -- ACCOUNT -- \n");
+		printf("2. Open a New Account\n");
+		printf("3. Exiting");
+        	printf("\nEnter your choice: ");
+        	scanf("%zu", &choice);
 
-        switch (choice) {
-        	case 1:   {
-			printf("Hello World !\n");
-           		  }
+        	switch (choice) {
+                	case 1: {
+                        	printf("Soon...");
+				//TODO
+				break;
+                        	}
 
-		case 2:   {
-			exit(0);
-			  }
+			case 2: {
+				printf("Soon...");
+				break;
+				}
 
-		default:
-			printf("Please try again\n");
-			}
+                	case 3: {
+				printf("Exiting...");
+                        	exit(0);
+                        	}
+
+                	default:
+                        	printf("Please try again\n");
+				break;
+                        	}
+	}
 }
